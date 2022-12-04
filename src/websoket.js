45cf528d492a85d86b804017ee17fa093ba2f1bd
neohral <honekingu29@gmail.com>
2022-12-04 20:23:31 +0900

    test

--- src/websoket.js
+++ src/websoket.js
@@ -1,6 +1,6 @@
 let host = window.document.location.host.replace(/:.*/, "");
 let ws = new WebSocket("wss://" + host + ":3002");
-document.getElementById("loginbtn").addEventListener("click", ()=>{document.getElementById("loginbtn").syle.visibility = "hidden"}, false);
+//document.getElementById("loginbtn").addEventListener("click", ()=>{document.getElementById("loginbtn").syle.visibility = "hidden"}, false);
 let send = mes => {
   ws.send(JSON.stringify(mes));
 };
